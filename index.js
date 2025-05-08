de6fed187b2b4433b59ee2831770b507a7b206be
sourav123-ws <sourav.bhattacherjee@webspiders.com>
2025-05-08 17:32:40 +0530

    added get-sponsor-meeting

--- index.js
+++ index.js
@@ -724,6 +724,29 @@ function send_sms_to_attendee(req, res) {
         });
 }
 
+function get_sponsors_meeting(req, res) {
+    let params = {
+        POST: {
+            methodToCall: user.getSponsorsMeeting,
+            methodNameText: "user.getSponsorsMeeting",
+            allowedRoles: [],
+            isPrivate: true,
+        },
+    };
+    let allowed_methods = Object.keys(params);
+    res = _set_cors(req, res, allowed_methods);
+    _handle_request(req, params)
+        .then((result) => {
+            if (result.status == 204) {
+                return res.status(204).send("");
+            }
+            return res.status(200).send(result);
+        })
+        .catch((err) => {
+            return res.status(200).send(err);
+        });
+}
+
 /* S U P P O R T I N G  M E T H O D S */
 function _set_cors(req, res, allowed_methods) {
     res.set("Access-Control-Allow-Origin", "*");
@@ -835,4 +858,5 @@ module.exports = {
     consolidatedSendEmail: consolidated_send_email,
     sendSMSToUser: send_sms_to_user,
     sendSMSToAttendee: send_sms_to_attendee,
+    getSponsorsMeeting : get_sponsors_meeting
 };
